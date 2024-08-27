abda08e5a7fcd09cd61e00c674b3166a8687f401
Deaa <deaarasheed77@gmail.com>
2024-08-27 17:13:26 +0200

    Auth with password

--- gundel/src/app/(auth)/signup/actions.ts
+++ gundel/src/app/(auth)/signup/actions.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import { lucia } from "@/auth";
 import prisma from "@/lib/prisma";
@@ -9,75 +9,75 @@ import { isRedirectError } from "next/dist/client/components/redirect";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export async function signUp (credentials : SignUpValues): Promise<{error:string}> {
+export async function signUp(
+  credentials: SignUpValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, email, password } = signUpSchema.parse(credentials);
 
-    try {
-        const {username, email, password} = signUpSchema.parse(credentials);
+    const passwordHash = await hash(password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
 
-        const passwordHash = await hash(password, {
-            memoryCost: 19456,
-            timeCost: 2,
-            outputLen: 32,
-            parallelism: 1
-        })
+    const userId = generateIdFromEntropySize(10);
 
-        const userId = generateIdFromEntropySize(10);
+    const existingUsername = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
 
-        const existingUsername = await prisma.user.findFirst({
-            where: {
-                username: {
-                    equals: username,
-                    mode: "insensitive",
-                }
-            }
-        })
-
-        if (existingUsername) {
-            return {
-                error: "username already taken",
-            }
-        }
-
-        const existingEmail = await prisma.user.findFirst({
-            where: {
-                email: {
-                    equals: email,
-                    mode: "insensitive",
-                }
-            }
-        })
-
-        if (existingEmail) {
-            return {
-                error: "email already taken",
-            }
-        }
-
-        await prisma.user.create({
-            data: {
-                id: userId,
-                username,
-                displayName: username,
-                email,
-                passwordHash
-            }
-        })
-
-        const session = await lucia.createSession(userId, {})
-        const sessionCookie = lucia.createSessionCookie(session.id);
-        cookies().set(
-            sessionCookie.name,
-            sessionCookie.value,
-            sessionCookie.attributes
-        );
-
-        return redirect("/")
-
-    } catch (error) {
-        if (isRedirectError(error)) throw error;
-        console.log(error);
-        return {
-            error: "smth went wrong"
-        }
+    if (existingUsername) {
+      return {
+        error: "username already taken",
+      };
     }
+
+    const existingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingEmail) {
+      return {
+        error: "email already taken",
+      };
+    }
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        displayName: username,
+        email,
+        passwordHash,
+      },
+    });
+
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.log(error);
+    return {
+      error: "smth went wrong",
+    };
+  }
 }
